c8d68fa60520d8c8d098ff995452e85d7ee816e0
Niti Nrw <niti.patel@flatironschool.com>
2016-11-03 21:32:49 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === stopValue) {
+      break;}
+      array[i] = changeValue;
+  }
+  return array
+  }
+
+  function keepGoing(array, changeValue, stopValue) {
+    for (let i=0; i < array.length; i++) {
+      if (array[i] === stopValue) {
+        continue;
+      }
+      array[i] = changeValue;
+    }
+    return array
+  }
+
+  function findBy(array, findFn)  {
+    for (let i = 0; i < array.length; i++ ) {
+      if (findFn(array[i])) {
+        return array[i]
+      }
+    }
+    return null
+  }
+
+// findBy(["dog", "cat", "cow"], a => a === "moose")
+
+
+
+// findBy(array, findFn) which looks for a value in array based on the return value of findFn. Return null if the value isn't found.
